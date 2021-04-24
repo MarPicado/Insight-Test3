ca24cba7b145ee4a890bbce00beef9ab41f55d01
achaves100 <adan.chaves@outlook.com>
2021-04-24 18:47:34 +0000

    initation

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -3,13 +3,57 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
+		<nav className="navbar style-nav justify-content-center">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<ul className="nav nav-pills style-navbutton ">
+					<li className="nav-item">
+						<a className="nav-link active" href="#">
+							Active
+						</a>
+					</li>
+					<li className="nav-item dropdown">
+						<a
+							className="nav-link dropdown-toggle"
+							data-toggle="dropdown"
+							href="#"
+							role="button"
+							aria-haspopup="true"
+							aria-expanded="false">
+							Dropdown
+						</a>
+						<div className="dropdown-menu">
+							<a className="dropdown-item" href="#">
+								Action
+							</a>
+							<a className="dropdown-item" href="#">
+								Another action
+							</a>
+							<a className="dropdown-item" href="#">
+								Something else here
+							</a>
+							<div className="dropdown-divider" />
+							<a className="dropdown-item" href="#">
+								Separated link
+							</a>
+						</div>
+					</li>
+					<li className="nav-item">
+						<a className="nav-link" href="#">
+							Link
+						</a>
+					</li>
+					<li className="nav-item">
+						<a className="nav-link disabled" href="#">
+							Disabled
+						</a>
+					</li>
+				</ul>
 			</Link>
 			<div className="ml-auto">
 				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
+					<span>
+						<i className="fas fa-shopping-cart fa-3x style-navcart" />
+					</span>
 				</Link>
 			</div>
 		</nav>
