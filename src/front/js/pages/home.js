ee96fbae9617a58c8de16d05bf61b5ba008e1fb0
achaves100 <adan.chaves@outlook.com>
2021-04-24 22:40:46 +0000

    update

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -72,6 +72,13 @@ export const Home = () => {
 					</li>
 				</ul>
 			</div>
+			<h3>
+				Section1 - Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+				labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+				ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
+				qui officia deserunt mollit anim id est laborum.
+			</h3>
 			<div className="section-2 box" />
 			<h3>
 				Section2 - Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
