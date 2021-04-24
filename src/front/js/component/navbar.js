220e3392486c50823fced6aa3c3d849de490c380
achaves100 <adan.chaves@outlook.com>
2021-04-24 20:35:42 +0000

    Carousel Update

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -4,51 +4,50 @@ import { Link } from "react-router-dom";
 export const Navbar = () => {
 	return (
 		<nav className="navbar style-nav justify-content-center fixed-top">
-			<Link to="/">
-				<ul className="nav nav-pills style-navbutton ">
-					<li className="nav-item">
-						<a className="nav-link active" href="#">
-							Active
+			<ul className="nav nav-pills style-navbutton ">
+				<li className="nav-item">
+					<a className="nav-link active" href="#">
+						Active
+					</a>
+				</li>
+				<li className="nav-item dropdown">
+					<a
+						className="nav-link dropdown-toggle"
+						data-toggle="dropdown"
+						href="#"
+						role="button"
+						aria-haspopup="true"
+						aria-expanded="false">
+						Dropdown
+					</a>
+					<div className="dropdown-menu">
+						<a className="dropdown-item" href="#">
+							Action
 						</a>
-					</li>
-					<li className="nav-item dropdown">
-						<a
-							className="nav-link dropdown-toggle"
-							data-toggle="dropdown"
-							href="#"
-							role="button"
-							aria-haspopup="true"
-							aria-expanded="false">
-							Dropdown
+						<a className="dropdown-item" href="#">
+							Another action
 						</a>
-						<div className="dropdown-menu">
-							<a className="dropdown-item" href="#">
-								Action
-							</a>
-							<a className="dropdown-item" href="#">
-								Another action
-							</a>
-							<a className="dropdown-item" href="#">
-								Something else here
-							</a>
-							<div className="dropdown-divider" />
-							<a className="dropdown-item" href="#">
-								Separated link
-							</a>
-						</div>
-					</li>
-					<li className="nav-item">
-						<a className="nav-link" href="#">
-							Link
+						<a className="dropdown-item" href="#">
+							Something else here
 						</a>
-					</li>
-					<li className="nav-item">
-						<a className="nav-link disabled" href="#">
-							Disabled
+						<div className="dropdown-divider" />
+						<a className="dropdown-item" href="#">
+							Separated link
 						</a>
-					</li>
-				</ul>
-			</Link>
+					</div>
+				</li>
+				<li className="nav-item">
+					<a className="nav-link" href="#">
+						Link
+					</a>
+				</li>
+				<li className="nav-item">
+					<a className="nav-link disabled" href="#">
+						Disabled
+					</a>
+				</li>
+			</ul>
+
 			<div className="ml-auto">
 				<Link to="/demo">
 					<span>
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -2,31 +2,79 @@ import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
+import { Carousel, Container } from "react-bootstrap";
 
 export const Home = () => {
 	return (
 		<div>
 			<div>
 				<div className="section-1 box" />
+				<Container>
+					<Carousel>
+						<Carousel.Item>
+							<img
+								className="d-block w-100"
+								src="https://cnnespanol.cnn.com/wp-content/uploads/2019/11/vegano.jpg?quality=100&strip=info"
+								alt="Vegano"
+								width={800}
+								height={800}
+							/>
+
+							<Carousel.Caption>
+								<h3>Platillos Veganos</h3>
+								<h4>Conozca las opciones para platillos veganos</h4>
+							</Carousel.Caption>
+						</Carousel.Item>
+						<Carousel.Item>
+							<img
+								className="d-block w-100"
+								src="https://saboryestilo.com.mx/wp-content/uploads/2019/05/carne-magra-de-res-cortes-saludables.jpg"
+								alt="Carnes"
+								width={800}
+								height={800}
+							/>
+
+							<Carousel.Caption>
+								<h3>Carnes</h3>
+								<h4>Deliciosas carnes a considerar</h4>
+							</Carousel.Caption>
+						</Carousel.Item>
+						<Carousel.Item>
+							<img
+								className="d-block w-100"
+								src="https://i1.wp.com/www.easytourchina.com/images/Photo/the-famous-chinese-spicy-dishes-how-to-cook-chicken-with-chilies/p363_d20140922161058_thumb_b.jpg"
+								alt="Picante"
+								width={800}
+								height={800}
+							/>
+
+							<Carousel.Caption>
+								<h3>Picantes</h3>
+								<h4>Comidas picantes</h4>
+							</Carousel.Caption>
+						</Carousel.Item>
+					</Carousel>
+				</Container>
+				<ul className="nav style-navcat justify-content-center">
+					<li className="nav-item">
+						<a className="nav-link active" href="#">
+							Vegano
+						</a>
+					</li>
+					<li className="nav-item">
+						<a className="nav-link" href="#">
+							Carnivoro
+						</a>
+					</li>
+					<li className="nav-item">
+						<a className="nav-link" href="#">
+							Picante
+						</a>
+					</li>
+				</ul>
 			</div>
 			<div className="section-2 box" />
-			<ul className="nav style-navcat justify-content-center">
-				<li className="nav-item">
-					<a className="nav-link active" href="#">
-						Vegan
-					</a>
-				</li>
-				<li className="nav-item">
-					<a className="nav-link" href="#">
-						Meet-Lover
-					</a>
-				</li>
-				<li className="nav-item">
-					<a className="nav-link" href="#">
-						Spicy
-					</a>
-				</li>
-			</ul>
+
 			<div className="section-3 box" />
 			<div className="section-4 box" />
 		</div>
